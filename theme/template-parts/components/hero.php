c70e4d095c5048847545c714b44ee075a5dd3839
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-06-09 13:00:41 -0400

    hero skeleton

---
+++ theme/template-parts/components/hero.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Hero Component
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package _tw
+ */
+
+?>
+
+<section class="hero stack wrapper">
+    <div class="hero-text stack text-center mx-auto">
+        <h1 class="eyebrow text-step-0 font-content mx-auto">HVAC Installation and Maintenance in North Carolina</h1>
+        <p class="header text-step-5 font-display"><span class="underline"><?php echo strtoupper("FULL-SERVICE"); ?></span> <?php echo strtoupper("HVAC FOR YOUR HOME AND BUSINESS"); ?></p>
+    </div>
+    <div class="hero__buttons mx-auto">
+        <a href="https://www.lebronconsulting.tech" class="btn outline-2 outline-primary">Lebron Consulting</a>
+        <a href="https://www.lebronconsulting.tech" class="btn bg-foreground text-background">Lebron Consulting</a>
+    </div>
+</section>
