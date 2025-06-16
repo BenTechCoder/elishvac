733c2101be7100125b9f5fcb15b8678cfc265e62
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-06-16 13:31:10 -0400

    skeleton Reviews (2)

--- theme/template-parts/components/reviews/review.php
+++ theme/template-parts/components/reviews/review.php
@@ -1,6 +1,8 @@
-<li class="card">
-    <div class="stars" style="--rating: <?php echo $args["stars"]?>;" aria-label="This review is a rating of <?php echo $args["stars"]?> stars out of 5."></div>
-    <h3><?php echo $args["name"]; ?></h3>
-    <?php echo $args["quote"]?>
-    <a href=" <?php echo $args["link"]?>">View Reveiw</a>
+<li class="card stack">
+    <div>
+        <div class="stars" style="--rating:<?php echo $args["stars"] ?>;" aria-label="This review is a rating of <?php echo $args["stars"] ?> stars out of 5."></div>
+        <h3><?php echo $args["name"]; ?></h3>
+        <?php echo $args["quote"] ?></p>
+    </div>
+    <a href=" <?php echo $args["link"] ?>">View Reveiw</a>
 </li>
