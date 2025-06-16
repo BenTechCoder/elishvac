01b49417c9ab137852bda287d30ff4699f096bac
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-06-16 12:39:08 -0400

    change services to grid

--- theme/template-parts/components/services-snippet.php
+++ theme/template-parts/components/services-snippet.php
@@ -4,7 +4,7 @@
         <p>Eli”s Heating and Air specializes in installation, repairs, and maintenance for electric, gas, dual-fuel, among other HVAC Units. Whether it's a new system or an emergency fix, we've got you covered.</p>
         <a href="" class="btn outline-2 outline-secondary">See all Services</a>
     </article>
-    <ul class="cluster">
+    <ul class="grid">
         <?php
         for ($i = 0; $i < 4; $i++) {
             get_template_part("template-parts/components/service-item");
