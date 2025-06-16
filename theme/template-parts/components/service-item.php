08aaf4c571181caaf8475c2502cf95490ec68350
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-06-16 13:37:28 -0400

    rename folder service-icons to icons

--- theme/template-parts/components/service-item.php
+++ theme/template-parts/components/service-item.php
@@ -1,4 +1,4 @@
 <li class="service__item card stack justify-center">
-    <img src=<?php echo get_parent_theme_file_uri("/assets/svg/services_icons/air-cooler.svg") ?> alt="Icon" class="icon">
+    <img src=<?php echo get_parent_theme_file_uri("/assets/svg/icons/air-cooler.svg") ?> alt="Icon" class="icon">
     <h3><?php echo strtoupper("cooling"); ?></h3>
 </li>
