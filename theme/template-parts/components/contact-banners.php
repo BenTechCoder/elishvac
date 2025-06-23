9a5b259cdd5e2613f9d944ebf768625d7c14d407
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-06-23 14:29:12 -0400

    contact banners skeleton

---
+++ theme/template-parts/components/contact-banners.php
@@ -0,0 +1,8 @@
+<section class="uppercase font-display text-step-3 md:text-step-4 lg:text-step-5 2xl:text-step-6 break-words">
+    <div class="py-m-l bg-secondary text-background">
+        <p class="wrapper text-center">Call <a href="tel:9192229005">919-222-9005</a> or <a href="/contact" class="btn bg-tertiary text-step-1">Contact us</a></p>
+    </div>
+    <div class="py-m-l bg-tertiary text-background text-center">
+        <a href="mailto:contact@elisheatingandair.com ">CONTACT@ELISHEATINGANDAIR.COM</a>
+    </div>
+</section>
