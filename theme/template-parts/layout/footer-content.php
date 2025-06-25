5b5ae4c74ffaf9e60e5508ab830bd0e17fabfb54
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-06-25 09:58:13 -0400

    skeleton Footer

--- theme/template-parts/layout/footer-content.php
+++ theme/template-parts/layout/footer-content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying the footer content
  *
@@ -9,24 +10,54 @@
 
 ?>
 
-<footer id="colophon" class="wrapper">
+<footer id="colophon" class="wrapper py-xl-2xl">
 
-	<?php if ( has_nav_menu( 'menu-2' ) ) : ?>
-		<nav aria-label="<?php esc_attr_e( 'Footer Menu', '_tw' ); ?>">
+	<?php if (has_nav_menu('menu-2')) : ?>
+		<nav aria-label="<?php esc_attr_e('Footer Menu', '_tw'); ?>" class="switcher">
+			<div>
+				<?php the_custom_logo(); ?>
+			</div>
+			<div>
+				<h2><?php echo strtoupper("contact"); ?></h2>
+				<ul class="stack">
+					<?php
+					$phone_number = get_option("business_details_phone_number");
+					$email = get_option("business_details_business_email");
+					?>
+					<li><a href="tel:<?php echo $phone_number; ?>"><?php echo $phone_number; ?></a></li>
+					<li><a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a></li>
+				</ul>
+			</div>
+			<div>
+				<h2><?php echo strtoupper("Links"); ?></h2>
+				<ul class="stack">
+					<?php
+					$footer_menu = wp_get_nav_menu_items("footer_menu");
+					foreach ($footer_menu as $item => $value) {
+					?>
+						<li><a href="<?php echo $value->url; ?>"><?php echo $value->title; ?></a></li>
+					<?php
+					}
+					?>
+				</ul>
+
+
+			</div>
 			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-2',
-					'menu_class'     => 'footer-menu',
-					'depth'          => 1,
-				)
-			);
+			// wp_nav_menu(
+			// 	array(
+			// 		'theme_location' => 'footer_menu',
+			// 		'menu_class'     => 'footer-menu',
+			// 		'depth'          => 1,
+			// 	)
+			// );
 			?>
 		</nav>
 	<?php endif; ?>
-
-	<div>
-	<p>Built By Lebron Consulting</p>
-	</div>
-
 </footer><!-- #colophon -->
+<div class="py-s bg-tertiary text-background">
+	<div class="wrapper stack center">
+		<p>®<?php echo date('Y'); ?> Eli's Heating and Air</p>
+		<p>Built By <a href="https://www.lebronconsulting.tech/">Lebron Consulting</a></p>
+	</div>
+</div>
