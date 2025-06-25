3603c39dc8aedec02e445f5f49749a7ff14db14e
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-06-25 10:13:30 -0400

    added custom fields to contact-banner

--- theme/template-parts/components/contact-banners.php
+++ theme/template-parts/components/contact-banners.php
@@ -1,8 +1,12 @@
+<?php
+$phone_number = get_option("business_details_phone_number");
+$email = get_option("business_details_business_email");
+?>
 <section class="uppercase font-display text-step-3 md:text-step-4 lg:text-step-5 2xl:text-step-6 break-words">
     <div class="py-m-l bg-secondary text-background">
-        <p class="wrapper text-center">Call <a href="tel:9192229005">919-222-9005</a> or <a href="/contact" class="btn bg-tertiary text-step-1">Contact us</a></p>
+        <p class="wrapper text-center">Call <a href="tel:<?php echo $phone_number; ?>"><?php echo strtoupper($phone_number); ?></a> or <a href="/contact" class="btn bg-tertiary text-step-1">Contact us</a></p>
     </div>
     <div class="py-m-l bg-tertiary text-background text-center">
-        <a href="mailto:contact@elisheatingandair.com ">CONTACT@ELISHEATINGANDAIR.COM</a>
+        <a href="mailto:<?php echo $email; ?>"><?php echo strtoupper($email); ?></a>
     </div>
 </section>
