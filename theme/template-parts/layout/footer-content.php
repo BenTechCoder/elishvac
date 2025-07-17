cc68f83b791e533fb5021a397910f62b2ff98278
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-07-17 16:32:38 -0400

    add google maps link to footer

--- theme/template-parts/layout/footer-content.php
+++ theme/template-parts/layout/footer-content.php
@@ -23,9 +23,11 @@
 					<?php
 					$phone_number = get_option("business_details_phone_number");
 					$email = get_option("business_details_business_email");
+					$google_maps_page = get_option("business_details_google_maps_page");
 					?>
 					<li><a href="tel:<?php echo $phone_number; ?>"><?php echo $phone_number; ?></a></li>
 					<li><a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a></li>
+					<li><a href="<?php echo $google_maps_page; ?>">Google Maps</a></li>
 				</ul>
 			</div>
 			<div>
@@ -44,9 +46,9 @@
 		</nav>
 	<?php endif; ?>
 	<div class="py-s bg-tertiary text-background">
-	<div class="wrapper stack text-center">
-		<p>®<?php echo date('Y'); ?> <?php echo get_option('business_details_legal_name'); ?></p>
-		<p>Built By <a href="https://www.lebronconsulting.tech/">Lebron Consulting</a></p>
+		<div class="wrapper stack text-center">
+			<p>®<?php echo date('Y'); ?> <?php echo get_option('business_details_legal_name'); ?></p>
+			<p>Built By <a href="https://www.lebronconsulting.tech/">Lebron Consulting</a></p>
+		</div>
 	</div>
-</div>
 </footer><!-- #colophon -->
