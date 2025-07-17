1c4a2a8c3d5e38225dd4cd333ea8e6b6d5a61ff4
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-07-17 09:14:52 -0400

    fix archive page not looping correctly

--- theme/archive.php
+++ theme/archive.php
@@ -21,24 +21,26 @@ get_template_part("template-parts/components/hero", null, array(
 ?>
 
 <section class="wrapper">
-	<article class="grid text-left py-3xl text-background">
+	<article class="grid text-left py-3xl text-background intersect-once intersect:motion-preset-fade-lg" style="--min-item-size: 450px;">
 
-		<?php if (have_posts()) : ?>
+		<?php if (have_posts()) :
+			while (have_posts()) : the_post();
+		?>
 
-			<?php
-			// Start the Loop.
-			?>
-			<div class="p-l stack bg-tertiary items-start intersect-once intersect:motion-translate-y-in-100">
-				<div>
-					<p><?php echo $post_type ?></p>
-					<h2 class="underline highlight"><?php echo get_the_title() ?><?php get_template_part("template-parts/components/highlight"); ?></h2>
+				<?php
+				// Start the Loop.
+				?>
+				<div class="p-l stack bg-tertiary items-start intersect-once intersect:motion-translate-y-in-100">
+					<div>
+						<p><?php echo $post_type ?></p>
+						<h2 class="underline highlight"><a href="<?php echo get_permalink() ?>"><?php echo get_the_title() ?><?php get_template_part("template-parts/components/highlight"); ?></a></h2>
+					</div>
+					<a href="<?php echo get_permalink() ?>" class="btn bg-secondary">See More</a>
 				</div>
-				<a href="<?php echo get_permalink() ?>" class="btn bg-secondary">See More</a>
-			</div>
 
 		<?php
-		// Previous/next page navigation
-
+			// Previous/next page navigation
+			endwhile;
 		else :
 
 			// If no content, include the "No posts found" template.
